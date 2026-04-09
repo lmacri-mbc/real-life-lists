4b2824ad7ab6337b48031f565eef158b85123040
Lee Macri <lee.macri@mbc.school.nz>
2026-04-10 10:27:39 +1200

    Add exercise functions for list operations

---
+++ exercises.py
@@ -0,0 +1,72 @@
+# exercises.py
+
+# 1. Create a list from 1 to 5
+def create_list():
+    # TODO: return [1, 2, 3, 4, 5]
+    pass
+
+
+# 2. Add 10 to the end of the list
+def add_ten(items):
+    # TODO: append 10 and return the list
+    pass
+
+
+# 3. Return the second element
+def second_element(items):
+    # TODO: return the element at index 1
+    pass
+
+
+# 4. Change the last element to 20
+def change_last_to_twenty(items):
+    # TODO: modify the last element and return the list
+    pass
+
+
+# 5. Remove the number 3
+def remove_three(items):
+    # TODO: remove 3 using a list method and return the list
+    pass
+
+
+# 6. Pop and return the first element
+def pop_first(items):
+    # TODO: pop index 0 and return (popped_value, updated_list)
+    pass
+
+
+# 7. Return the length of the list
+def list_length(items):
+    # TODO: return len(items)
+    pass
+
+
+# 8. Concatenate with [6, 7, 8]
+def concatenate_lists(items):
+    # TODO: return items + [6, 7, 8]
+    pass
+
+
+# 9. Check if 5 is in the list
+def contains_five(items):
+    # TODO: return True if 5 in items else False
+    pass
+
+
+# 10. Reverse the list
+def reverse_list(items):
+    # TODO: return the reversed list
+    pass
+
+
+# 11. Sort in descending order
+def sort_descending(items):
+    # TODO: return the list sorted from largest to smallest
+    pass
+
+
+# 12. Remove duplicates from [2, 2, 3, 3, 4]
+def remove_duplicates(items):
+    # TODO: return a list with duplicates removed
+    pass
